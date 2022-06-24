2dde29fe8431a285c27ce95714d37cf996c1a5b7
mio kataoka <mio.kanna@gmail.com>
2022-06-23 20:14:49 -0700

    commit tree f9d617a6c629c3fb3aa9e3ef285b11e3033c26bd

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_arr = self.split(/\.|\?|!/)
+    sentence_arr.filter { |sentence| !sentence.empty? }.size
   end
 end
